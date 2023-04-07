a3047dbef3d9bbd6130a879427a36c8150ca3480
sweetpickleswine <80652149+HeronErin@users.noreply.github.com>
2023-04-06 21:50:43 -0400

    Update train_tacotron_dca.py

--- recipes/ljspeech/tacotron2-DCA/train_tacotron_dca.py
+++ recipes/ljspeech/tacotron2-DCA/train_tacotron_dca.py
@@ -1,5 +1,5 @@
-import os
-
+import os, sys
+contd = (sys.argv[-1] if len(sys.argv) == 2 else None)
 from trainer import Trainer, TrainerArgs
 
 from TTS.config.shared_configs import BaseAudioConfig
@@ -94,7 +94,7 @@ model = Tacotron2(config, ap, tokenizer)
 # Trainer provides a generic API to train all the 🐸TTS models with all its perks like mixed-precision training,
 # distributed training, etc.
 trainer = Trainer(
-    TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
+    TrainerArgs(continue_path=contd), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
 )
 
 # AND... 3,2,1... 🚀
